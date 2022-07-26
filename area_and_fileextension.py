dffe9f688285b1004b12ad1f039eb0de64897950
roshini6646 <96617518+roshini6646@users.noreply.github.com>
2022-07-26 18:54:34 +0530

    Create area_and_fileextension.py

---
+++ area_and_fileextension.py
@@ -0,0 +1,25 @@
+# write a Python program which accepts the radius of a circle from the user and computes area.
+def area_of_circle(r):
+  print( 3.14 * r * r)
+    
+area_of_circle(3)
+28.25999999999999
+area_of_circle(1.1)
+3.7994000000000008
+
+
+
+
+#  write a Python program to accept a filename from the user and print the extension of that.
+file_name = input("name:")
+name:abc.py
+file_name
+'abc.py'
+a = file_name.endswith(".py")
+if a is True :
+    print("The extension of the file is :'python'")
+else :
+    print("The extension of the file is not python")
+
+    
+The extension of the file is :'python'
